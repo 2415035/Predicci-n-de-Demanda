cd560d2f9c6220cbd2ca25265c5bb1174a9c11f9
2415035 <i2415035@continental.edu.pe>
2024-12-22 18:54:28 +0000

    Agregado modelo predictivo con interfaz Streamlit

--- app.py
+++ app.py
@@ -1,28 +1,43 @@
 import pandas as pd
 import streamlit as st
+import csv
 
 # Configuración de la aplicación
 st.title('Modelo Predictivo de Demanda de Envíos')
 st.write("Cargar los datasets para analizar la demanda de envíos.")
 
+# Función para detectar delimitador
+def detectar_delimitador(file):
+    try:
+        sample = file.read(1024).decode()  # Leer una muestra del archivo
+        file.seek(0)  # Volver al inicio del archivo
+        sniffer = csv.Sniffer()
+        delimitador = sniffer.sniff(sample).delimiter
+        return delimitador
+    except Exception as e:
+        st.error(f"Error al detectar delimitador: {e}")
+        return None
+
 # Subir los archivos CSV
-dataset_1_file = st.file_uploader("Subir el primer dataset (delimitado por ;)", type=["csv"])
-dataset_2_file = st.file_uploader("Subir el segundo dataset (delimitado por ;)", type=["csv"])
+dataset_1_file = st.file_uploader("Subir el primer dataset", type=["csv"])
+dataset_2_file = st.file_uploader("Subir el segundo dataset", type=["csv"])
 
 # Procesar el primer archivo si se carga
 if dataset_1_file is not None:
-    try:
-        dataset_1 = pd.read_csv(dataset_1_file, sep=";", quotechar='"', on_bad_lines='skip')  # Leer con delimitador ;
+    delimitador_1 = detectar_delimitador(dataset_1_file)
+    if delimitador_1:
+        dataset_1 = pd.read_csv(dataset_1_file, sep=delimitador_1, quotechar='"', on_bad_lines='skip')
         st.write("Primer dataset cargado con éxito:")
-        st.write(dataset_1.head())  # Mostrar las primeras 5 filas
-    except Exception as e:
-        st.error(f"Error al cargar el primer dataset: {e}")
+        st.write(dataset_1.head())
+    else:
+        st.error("No se pudo detectar el delimitador del primer dataset.")
 
 # Procesar el segundo archivo si se carga
 if dataset_2_file is not None:
-    try:
-        dataset_2 = pd.read_csv(dataset_2_file, sep=";", quotechar='"', on_bad_lines='skip')  # Leer con delimitador ;
+    delimitador_2 = detectar_delimitador(dataset_2_file)
+    if delimitador_2:
+        dataset_2 = pd.read_csv(dataset_2_file, sep=delimitador_2, quotechar='"', on_bad_lines='skip')
         st.write("Segundo dataset cargado con éxito:")
-        st.write(dataset_2.head())  # Mostrar las primeras 5 filas
-    except Exception as e:
-        st.error(f"Error al cargar el segundo dataset: {e}")
+        st.write(dataset_2.head())
+    else:
+        st.error("No se pudo detectar el delimitador del segundo dataset.")
