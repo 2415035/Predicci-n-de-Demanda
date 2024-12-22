96a5bec41bb810a4746e693434564095d6fed6f4
2415035 <i2415035@continental.edu.pe>
2024-12-22 18:11:49 +0000

    Agregado modelo predictivo con interfaz Streamlit

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import streamlit as st
 
-# Subir los archivos CSV
+# Configuración de la aplicación
 st.title('Modelo Predictivo de Demanda de Envíos')
 st.write("Cargar los datasets para analizar la demanda de envíos.")
 
+# Subir los archivos CSV
 dataset_1_file = st.file_uploader("Subir el primer dataset", type=["csv"])
 dataset_2_file = st.file_uploader("Subir el segundo dataset", type=["csv"])
 
-# Mostrar las primeras 5 filas del dataset
-print(dataset_1_file.head())
+# Procesar el primer archivo si se carga
+if dataset_1_file is not None:
+    dataset_1 = pd.read_csv(dataset_1_file)  # Leer el archivo como dataframe
+    st.write("Primer dataset cargado con éxito:")
+    st.write(dataset_1.head())  # Mostrar las primeras 5 filas
+
+# Procesar el segundo archivo si se carga
+if dataset_2_file is not None:
+    dataset_2 = pd.read_csv(dataset_2_file)  # Leer el archivo como dataframe
+    st.write("Segundo dataset cargado con éxito:")
+    st.write(dataset_2.head())  # Mostrar las primeras 5 filas
