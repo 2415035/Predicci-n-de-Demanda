bbdbec8932163f041844fd121ca1a00795fdc305
2415035 <i2415035@continental.edu.pe>
2024-12-22 13:46:50 +0000

    xxx

--- predictive_model.py
+++ predictive_model.py
@@ -4,6 +4,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import matplotlib.pyplot as plt
+import os
+
+file_path = "Amazon Sale Report.csv"
+
+if os.path.exists(file_path):
+    print(f"El archivo '{file_path}' existe.")
+    print(f"Tamaño del archivo: {os.path.getsize(file_path)} bytes")
+else:
+    print(f"El archivo '{file_path}' no existe.")
 
 # Cargar datasets
 dataset_1 = pd.read_csv("Amazon Sale Report.csv")  # Primer dataset
