f9b9fc4a814cd9143c08cd719892a97bab3660f3
2415035 <i2415035@continental.edu.pe>
2024-12-22 18:52:43 +0000

    Agregado modelo predictivo con interfaz Streamlit

--- app.py
+++ app.py
@@ -6,27 +6,23 @@ st.title('Modelo Predictivo de Demanda de Envíos')
 st.write("Cargar los datasets para analizar la demanda de envíos.")
 
 # Subir los archivos CSV
-dataset_1_file = st.file_uploader("Subir el primer dataset", type=["csv"])
-dataset_2_file = st.file_uploader("Subir el segundo dataset", type=["csv"])
+dataset_1_file = st.file_uploader("Subir el primer dataset (delimitado por ;)", type=["csv"])
+dataset_2_file = st.file_uploader("Subir el segundo dataset (delimitado por ;)", type=["csv"])
 
 # Procesar el primer archivo si se carga
 if dataset_1_file is not None:
     try:
-        dataset_1 = pd.read_csv(dataset_1_file, sep=";", on_bad_lines='skip')  # Manejar errores de líneas
+        dataset_1 = pd.read_csv(dataset_1_file, sep=";", quotechar='"', on_bad_lines='skip')  # Leer con delimitador ;
         st.write("Primer dataset cargado con éxito:")
-        st.write("Dimensiones del dataset:", dataset_1.shape)
-        st.write("Primeras filas del dataset:")
         st.write(dataset_1.head())  # Mostrar las primeras 5 filas
     except Exception as e:
-        st.error(f"Error al leer el primer dataset: {e}")
+        st.error(f"Error al cargar el primer dataset: {e}")
 
 # Procesar el segundo archivo si se carga
 if dataset_2_file is not None:
     try:
-        dataset_2 = pd.read_csv(dataset_2_file, sep=",", on_bad_lines='skip')  # Manejar errores de líneas
+        dataset_2 = pd.read_csv(dataset_2_file, sep=";", quotechar='"', on_bad_lines='skip')  # Leer con delimitador ;
         st.write("Segundo dataset cargado con éxito:")
-        st.write("Dimensiones del dataset:", dataset_2.shape)
-        st.write("Primeras filas del dataset:")
         st.write(dataset_2.head())  # Mostrar las primeras 5 filas
     except Exception as e:
-        st.error(f"Error al leer el segundo dataset: {e}")
+        st.error(f"Error al cargar el segundo dataset: {e}")
