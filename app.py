a47ac0c9c29732a6581a185899efc8e706767280
2415035 <i2415035@continental.edu.pe>
2025-01-19 23:37:12 +0000

    Hola

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from math import sqrt
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -56,7 +57,7 @@ if uploaded_file:
 
     # Métricas de regresión
     mse = mean_squared_error(y_test, y_pred)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
+    rmse = sqrt(mean_squared_error(y_test, y_pred))
     mae = mean_absolute_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
     r2_percentage = r2 * 100
@@ -114,4 +115,5 @@ if uploaded_file:
     ax.set_ylabel("Pedidos Predichos")
     ax.set_title("Predicciones Reales vs Predichas")
     st.pyplot(fig)
+
 
